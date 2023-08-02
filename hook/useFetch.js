a3e27c2a93a3dd28c16d245e556e753f0b34d3f1
Adinath Shelke <shelkeadinath3@gmail.com>
2023-08-02 19:19:41 +0530

    successfully fetching jobs using apo

---
+++ hook/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setdata] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [error, seterror] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "326795b354msh3dddb826bdf45b4p12e012jsn7ce75fc141da",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+  const fetchData = async () => {
+    setisLoading(true);
+    try {
+      const response = await axios.request(options);
+      setdata(response.data.data);
+      setisLoading(false);
+    } catch (error) {
+      seterror(error);
+      alert("Error occured!");
+    } finally {
+      setisLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setisLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
