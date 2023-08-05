73fc1477a760b1dd5289bb00191336bf7a6655d7
Adinath Shelke <shelkeadinath3@gmail.com>
2023-08-05 14:44:18 +0530

    job details done

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -18,9 +18,37 @@ import {
 } from "../../components";
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
+
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 const JobDetails = () => {
   const [refreshing, setrefreshing] = useState(false);
+  const [activeTab, setactiveTab] = useState(tabs[0]);
   const onRefresh = () => {};
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data available"} />
+        );
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+      default:
+        break;
+    }
+  };
   const params = useSearchParams();
   const router = useRouter();
 
@@ -64,11 +92,29 @@ const JobDetails = () => {
             <Text>No data available</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-              <Company />
-              <JobTabs />
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                country={data[0].job_country}
+                city={data[0].job_city}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setactiveTab}
+              />
+
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
       </>
     </SafeAreaView>
   );
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from "react-native";
+import { icons } from "../../../constants";
+import styles from "./company.style";
+import { checkiImageURL } from "../../../utils";
 
-import styles from './company.style'
-
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, country, city }) => {
   return (
-    <View>
-      <Text>Company</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkiImageURL(companyLogo)
+              ? companyLogo
+              : "https://static.thenounproject.com/png/1551321-200.png",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
 
-export default Company
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName}/</Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>
+            {country} ({city})
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Company;
